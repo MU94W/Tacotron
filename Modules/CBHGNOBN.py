036f2a00863ae5186e10d7dabe30bfd93ff43135
Wang Mu <nanqiao15@126.com>
2017-07-11 23:43:22 +0800

    no batch_norm version

---
+++ Modules/CBHGNOBN.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+from tensorflow.python.ops import array_ops
+from TFCommon.RNNCell import GRUCell
+from Tacotron.Modules import ConvNetNOBN, HighwayNet
+
+bidirectional_dynamic_rnn = tf.nn.bidirectional_dynamic_rnn
+
+Conv1dBankWithMaxPool   = ConvNetNOBN.Conv1dBankWithMaxPool
+Conv1dProjection        = ConvNetNOBN.Conv1dProjection
+FCHighwayNet            = HighwayNet.FCHighwayNet
+
+class CBHG(object):
+    """CBHG Net
+    """
+    
+    def __init__(self, bank_K, proj_unit, highway_layers=4):
+        """
+        Args:
+            bank_K: int
+            proj_unit: a pair of int
+        """
+        self.__bank_K = bank_K
+        self.__proj_unit = proj_unit
+        self.__highway_layers = highway_layers
+
+    @property
+    def bank_K(self):
+        return self.__bank_K
+
+    @property
+    def proj_unit(self):
+        return self.__proj_unit
+
+    @property
+    def highway_layers(self):
+        return self.__highway_layers
+
+    def __call__(self, inputs, sequence_length=None, is_training=True, time_major=None):
+        assert time_major is not None, "[*] You must specify whether is time_major or not!"
+        if time_major:
+            inputs = tf.transpose(inputs, perm=(1,0,2))     # Use batch major data.
+        assert inputs.get_shape()[-1] == self.proj_unit[1], "[!] input's shape is not the same as ConvProj's output!"
+
+        ### for correctness.
+        if sequence_length is not None:
+            mask = tf.expand_dims(array_ops.sequence_mask(sequence_length, tf.shape(inputs)[1], tf.float32), -1)
+            inputs = inputs * mask
+
+        ConvBankWithPool    = Conv1dBankWithMaxPool(self.bank_K)
+        ConvProj            = Conv1dProjection(self.proj_unit)
+        Highway             = FCHighwayNet(self.highway_layers)
+        rnn_cell_fw         = GRUCell(self.proj_unit[1])
+        rnn_cell_bw         = GRUCell(self.proj_unit[1])
+
+        ### calculate
+        # conv net
+        output_0 = ConvBankWithPool(inputs, is_training)
+
+        ### for correctness.
+        if sequence_length is not None:
+            output_0 = output_0 * mask
+
+        output_1 = ConvProj(output_0, is_training)
+        # residual connect
+        res_output = tf.identity(inputs) + output_1
+
+        # highway net
+        highway_output = Highway(res_output)
+
+        # biGRU
+        # batch major
+        final_output, *_ = bidirectional_dynamic_rnn(rnn_cell_fw, rnn_cell_bw, highway_output, sequence_length=sequence_length, time_major=False, dtype=tf.float32)
+        final_output = tf.concat(final_output, axis=-1)
+        if time_major:
+            final_output = tf.transpose(final_output, perm=(1,0,2))
+
+        return final_output
+
---
+++ Modules/ConvNetNOBN.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+from six.moves import xrange
+
+
+def __conv1d__(inputs, width, stride, in_channels, out_channels):
+    filter_1d = tf.get_variable(name='filter', shape=(width, in_channels, out_channels))
+    return tf.nn.conv1d(inputs, filter_1d, stride, 'SAME')
+
+def __conv1d_alone_time__(inputs, width, in_channels, out_channels):
+    return __conv1d__(inputs, width, 1, in_channels, out_channels)
+
+class Conv1dBankWithMaxPool(object):
+    """Conv1d Bank.
+    The output is max_pooled along time.
+    """
+
+    def __init__(self, K, activation=tf.nn.relu):
+        self.__K = K
+        self.__activation = activation
+
+    @property
+    def K(self):
+        return self.__K
+
+    @property
+    def activation(self):
+        return self.__activation
+
+    def __call__(self, inputs, is_training=True, scope=None):
+        """
+        Args:
+            inputs: with shape -> (batch_size, time_step/width, units/channels)
+        """
+        with tf.variable_scope(scope or type(self).__name__):
+            in_channels = inputs.shape[-1].value
+            conv_lst = []
+            for idk in xrange(1, self.K + 1):
+                with tf.variable_scope('inner_conv_%d' % idk):
+                    conv_k = self.activation(__conv1d_alone_time__(inputs, idk, in_channels, in_channels))
+                conv_lst.append(conv_k)
+
+            stacked_conv = tf.stack(conv_lst, axis=-1)   # shape -> (batch_size, time_step/width, units/channels, K/height)
+            #re_shape = tf.shape(stacked_conv)[:2] + [1, in_channels * self.K]
+            re_shape = [tf.shape(stacked_conv)[0], tf.shape(stacked_conv)[1], 1, in_channels * self.K]
+            stacked_conv = tf.reshape(stacked_conv, shape=re_shape)     # shape -> (batch_size, time_step/width, 1, units*K/channels)
+
+            ### max pool along time
+            ksize = [1, 2, 1, 1]
+            strid = [1, 1, 1, 1]
+            pooled_conv = tf.squeeze(tf.nn.max_pool(stacked_conv, ksize, strid, 'SAME'), axis=2)    # shape -> (batch_size, time_step/width, units*K/channels)
+
+            return pooled_conv
+
+class Conv1dProjection(object):
+    """Conv1d Projection
+    """
+
+    def __init__(self, proj_unit, width=3, activation=tf.nn.relu):
+        self.__proj_unit = proj_unit
+        self.__width = width
+        self.__activation = activation
+
+    @property
+    def proj_unit(self):
+        return self.__proj_unit
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def activation(self):
+        return self.__activation
+
+    def __call__(self, inputs, is_training=True, scope=None):
+        """
+        Args:
+            inputs: with shape -> (batch_size, time_step/width, units/channels)
+        """
+        with tf.variable_scope(scope or type(self).__name__):
+            filter_width = self.width
+            proj_0 = self.proj_unit[0]
+            proj_1 = self.proj_unit[1]
+            in_channels = inputs.get_shape()[-1].value
+            with tf.variable_scope('inner_conv_with_acti'):
+                conv_a = self.activation(__conv1d_alone_time__(inputs, filter_width, in_channels, proj_0))
+            with tf.variable_scope('inner_conv_linear'):
+                conv_l = __conv1d_alone_time__(conv_a, filter_width, proj_0, proj_1)
+
+            return conv_l
+
